1773a48938796875cc82a46320281ab6f6c68b79
Terry Lorber <terry@soiboston.com>
2018-03-05 22:44:08 -0500

    Use instance variable for years.

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/' do
+    @districts = DistrictDimension.all 
+    @grades = GradeDimension.all
+   
     @district = DistrictDimension.first(name: params[:district])
     @grade = GradeDimension.first(name: params[:grade])
 
@@ -13,6 +16,9 @@ class App < Sinatra::Base
     @rows = EnrollmentFact.all(district_dimension_id: @district.id,
                                grade_dimension_id: @grade.id,
                                order: [ :time_dimension_id.desc ])
+
+    @years = @rows.collect{|r| r.time_dimension.year}
+                                        
     haml :index
   end
   
