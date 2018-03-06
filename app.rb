b019b4c1a48d2b96ed9ad8bd2a557136f73fbf39
Terry Lorber <terry@soiboston.com>
2018-03-05 22:27:23 -0500

    Allow for parameters.

--- app.rb
+++ app.rb
@@ -4,10 +4,14 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/' do
-    @district = 'Swampscott'
-    
-    @rows = EnrollmentFact.all(district_dimension_id: 200,
-                               grade_dimension_id: 2,
+    @district = DistrictDimension.first(name: params[:district])
+    @grade = GradeDimension.first(name: params[:grade])
+
+    @district = DistrictDimension.first(name: 'Swampscott') if @district.nil?
+    @grade = GradeDimension.first(name: 'K') if @grade.nil? 
+   
+    @rows = EnrollmentFact.all(district_dimension_id: @district.id,
+                               grade_dimension_id: @grade.id,
                                order: [ :time_dimension_id.desc ])
     haml :index
   end
