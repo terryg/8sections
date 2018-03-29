1b245821a8f30da4587a2042c884fe343bffc14b
Terry Lorber <tgl@rideside.net>
2018-03-28 23:31:02 -0400

    Update init.rb

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ require 'sinatra'
 configure { set :server, :puma }
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_GRAY'])
+DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_GRAY_URL'])
 
 require './models/enrollment_fact'
 
