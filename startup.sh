f5df1a806acaf99b020971bda1a2b6e274f1f951
Terry Lorber <tgl@rideside.net>
2018-11-04 08:20:00 -0500

    Fixup startup script.

--- startup.sh
+++ startup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-gem install bundler
-gem install sinatra
-gem install puma
 source .env
+
 if [ "$RACK_ENV" == "production" ]; 
 then 
-  bundle install --without development test
+  bundle install --without development test --path vendor/bundle
   bundle exec puma -C config/puma.rb
 else
-  bundle install
+  bundle install --path vendor/bundle
   if [ "$RACK_ENV" == "test" ]; 
   then
     rspec
   else
-    gem install shotgun
-    shotgun -I /usr/src/app $MAIN_APP_FILE -p 8000 -o '0.0.0.0'
+    bundle exec shotgun -p 8000 -o '0.0.0.0' -O
   fi
 fi
