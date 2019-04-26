86d9cc4c6a0ba65296849aabc39212b1e5fb0d8b
Terry Lorber <terry@metatooth.com>
2019-04-25 22:28:36 -0400

    Start implementation of rake db:seed

--- models/grade_dimension.rb
+++ models/grade_dimension.rb
@@ -2,5 +2,6 @@ class GradeDimension
   include DataMapper::Resource
 
   property :id, Serial, :index => true
+  property :sequence, Integer, :required => true
   property :name, String, :length => 80
 end
